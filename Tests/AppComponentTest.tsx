830002af83d20695dbd39c667f04017fb6cc298b
kamthamc <kamthamc@gmail.com>
2017-05-07 03:38:20 -0700

    coverage

---
+++ Tests/AppComponentTest.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import * as ReactTestRenderer from 'react-test-renderer';
+
+
+import { AppComponent } from '../src/AppComponent';
+
+
+describe('AppComponent', () => {
+
+    let appComponent;
+
+
+    beforeEach(function () {
+        appComponent = ReactTestRenderer.create(<AppComponent />);
+    });
+
+    test('initialization', () => {
+        let tree = appComponent.toJSON();
+        expect(tree).toMatchSnapshot();
+        expect(tree.props.className).toBe('alpha');
+    });
+
+});
+
